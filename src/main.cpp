a9048578a39b2d80e034971260efeed5a1191920
Acidic <ares.kittell@gmail.com>
2026-03-10 15:39:09 -0400

    added assimp and compile.sh

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <glad/glad.h>
+#include <glad/glad.c>
 #include <GLFW/glfw3.h>
-#include <stb_image.h>
 
+#include <stb_image.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
+#include <stb_image.cpp>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
